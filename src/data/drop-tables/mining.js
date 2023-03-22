ff673dbdb90aec9112d21903508d50c30de0b66b
AwwCookies <emma@emaw.dev>
2023-03-22 08:13:02 -0400

    nightly

--- src/data/drop-tables/mining.js
+++ src/data/drop-tables/mining.js
@@ -1,32 +1,32 @@
-import { getItemByName } from "@/items/items";
+import { getItemByName, getItemsByName } from "@/items/items";
 
-export const common = [
-    getItemByName("Copper Ore"),
-    getItemByName("Gold Ore"),
-    getItemByName("Silver Ore"),
-    getItemByName("Coal"),
-    getItemByName("Iron Ore")
-]
+export const common = getItemsByName([
+    "Copper Ore",
+    "Gold Ore",
+    "Silver Ore",
+    "Coal",
+    "Iron Ore"
+])
 
-export const uncommon = [
-    getItemByName("Fossil"),
-    getItemByName("Onyx"),
-    getItemByName("Amethyst"),
-    getItemByName("Topaz")
-]
+export const uncommon = getItemsByName([
+    "Fossil",
+    "Onyx",
+    "Amethyst",
+    "Topaz"
+])
 
-export const rare = [
-    getItemByName("Diamond"),
-    getItemByName("Ruby"),
-    getItemByName("Emerald"),
-    getItemByName("Sapphire"),
-    getItemByName("Platinum"),
-    getItemByName("Aquamarine"),
-    getItemByName("Garnet"),
-    getItemByName("Morganite"),
-    getItemByName("Geode"),
-    getItemByName("Artifact")
-]
+export const rare = getItemsByName([
+    "Diamond",
+    "Ruby",
+    "Emerald",
+    "Sapphire",
+    "Platinum",
+    "Aquamarine",
+    "Garnet",
+    "Morganite",
+    "Geode",
+    "Artifact"
+])
 
 export const legendary = [
     getItemByName("Treasure Chest")
